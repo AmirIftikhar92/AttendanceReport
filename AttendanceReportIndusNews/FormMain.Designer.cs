b727f327de7511b41f32653513868a2581321f3c
Muhammad Amir Iftikhar <amir.iftikhar@AMN.PK>
2020-11-19 17:17:49 +0500

    Add project files.

---
+++ AttendanceReportIndusNews/FormMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AttendanceReportIndusNews
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonImportFile = new System.Windows.Forms.Button();
+            this.DataGridView_Excel = new System.Windows.Forms.DataGridView();
+            this.OpenFileDialog_Excel = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Excel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonImportFile
+            // 
+            this.ButtonImportFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonImportFile.Location = new System.Drawing.Point(12, 12);
+            this.ButtonImportFile.Name = "ButtonImportFile";
+            this.ButtonImportFile.Size = new System.Drawing.Size(90, 34);
+            this.ButtonImportFile.TabIndex = 0;
+            this.ButtonImportFile.Text = "&Import File";
+            this.ButtonImportFile.UseVisualStyleBackColor = true;
+            this.ButtonImportFile.Click += new System.EventHandler(this.ButtonImportFile_Click);
+            // 
+            // DataGridView_Excel
+            // 
+            this.DataGridView_Excel.AllowUserToAddRows = false;
+            this.DataGridView_Excel.AllowUserToDeleteRows = false;
+            this.DataGridView_Excel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_Excel.Location = new System.Drawing.Point(13, 53);
+            this.DataGridView_Excel.Name = "DataGridView_Excel";
+            this.DataGridView_Excel.ReadOnly = true;
+            this.DataGridView_Excel.Size = new System.Drawing.Size(759, 496);
+            this.DataGridView_Excel.TabIndex = 1;
+            // 
+            // OpenFileDialog_Excel
+            // 
+            this.OpenFileDialog_Excel.FileName = "Excel File";
+            this.OpenFileDialog_Excel.Filter = "\"Excel Files|*.xls; *.xlsx\"";
+            this.OpenFileDialog_Excel.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog_Excel_FileOk);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.DataGridView_Excel);
+            this.Controls.Add(this.ButtonImportFile);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Attendance Report | Indus News";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_Excel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonImportFile;
+        private System.Windows.Forms.DataGridView DataGridView_Excel;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog_Excel;
+    }
+}
+
---
+++ AttendanceReportIndusNews/FormMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttendanceReportIndusNews
+{
+    public partial class FormMain : Form
+    {
+        private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonImportFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog_Excel.ShowDialog();
+        }
+
+        public void OpenFileDialog_Excel_FileOk(object sender, CancelEventArgs e)
+        {
+            string filePath = OpenFileDialog_Excel.FileName;
+            string extension = Path.GetExtension(filePath);
+            string header = "YES";
+            string conStr, sheetName;
+
+            conStr = string.Empty;
+            switch (extension)
+            {
+                case ".xls": //EXCEL 97-03
+                    conStr = string.Format(Excel03ConString, filePath, header);
+                    break;
+
+                case ".xlsx": //EXCEL 07-HIGHER
+                    conStr = string.Format(Excel07ConString, filePath, header);
+                    break;
+            }
+
+            //Get name of the first sheet of Excel file
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                    con.Close();
+                }
+            }
+
+            //Read data from the first sheet of Excel file
+            using (OleDbConnection con = new OleDbConnection(conStr))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    {
+                        DataTable dt = new DataTable();
+                        cmd.CommandText = "SELECT * FROM ["+ sheetName +"]";
+                        cmd.Connection = con;
+                        con.Open();
+                        oda.SelectCommand = cmd;
+                        oda.Fill(dt);
+                        con.Close();
+
+                        dt = dt.DefaultView.ToTable(true, "Ac-No", "Name", "sTime", "sDate");
+                        DataGridView_Excel.DataSource = dt;
+
+                        //Group Employee Id and DateTime
+                        var date = dt.AsEnumerable().GroupBy(s => s.Field<DateTime>("sDate")).ToList();
+                        var employeeId = dt.AsEnumerable().GroupBy(s => s.Field<string>("Ac-No"));
+                        var byAccount = employeeId.ToDictionary(g => g.Key, g => g.Select(s => s.Field<DateTime>("sTime").ToString("MM/dd/yyyy HH:mm")).ToArray());
+
+                        DataTable dataTable = new DataTable();
+                        dataTable.Columns.Add("Id");
+                        dataTable.Columns.Add("Check-In");
+                        dataTable.Columns.Add("Check-Out");
+                        dataTable.Columns.Add("Duration");
+                        foreach (var item in byAccount)
+                        {
+                            for (int i = 0; i < date.Count; i++)
+                            {
+                                DataRow dataRow = dataTable.NewRow();
+                                dataRow["Id"] = item.Key;
+                                dataRow["Check-In"] = item.Value.Min();
+                                dataRow["Check-Out"] = item.Value.Max();
+                                dataTable.Rows.Add(dataRow);
+
+                                DataGridView_Excel.DataSource = dataTable;
+                            }
+                            
+                        }
+
+                       
+                    }
+                }
+            }
+        }
+    }
+}
